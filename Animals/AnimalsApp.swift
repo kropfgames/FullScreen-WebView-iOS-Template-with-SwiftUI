5d691bf8eca4d8d955e5f06d72646eff104f728a
Alexandra Kropf <52729602+kropfgames@users.noreply.github.com>
2022-08-21 21:27:27 -0700

    Initial Commit

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Alex on 2022-08-21.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
